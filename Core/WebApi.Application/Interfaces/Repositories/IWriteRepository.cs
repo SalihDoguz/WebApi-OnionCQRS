becc43bbd8658523fce2d53f58ad78896d74a65a
SalihDoguz <salihdoguz01@gmail.com>
2024-02-22 20:34:27 +0300

    IWriteRepository soyut somut yazıldı

--- Core/WebApi.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/WebApi.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApi.Application.Interfaces.Repositories
+﻿using WebApi.Domain.Common;
+
+namespace WebApi.Application.Interfaces.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class ,IEntityBase,new()
     {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+
     }
 }
